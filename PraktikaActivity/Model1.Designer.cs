8c80137b340277517a39974ebf7bac37ef914870
Almaz <52231348+Heahdix@users.noreply.github.com>
2024-05-08 16:32:29 +0300

    l

--- PraktikaActivity/Model1.Designer.cs
+++ PraktikaActivity/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\Almaz\source\repos\PraktikaActivity\PraktikaActivity\ActivityModel.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\Almaz\source\repos\PraktikaActivity\PraktikaActivity\Model1.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
